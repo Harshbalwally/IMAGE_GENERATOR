c5ee50c981bf01e5f4d97b3e28de7b0f67b97f95
Harshbalwally <harshbalwally907@gmail.com>
2024-12-28 06:30:54 +0530

    push front end code

--- src/components/GenerateImageForm.jsx
+++ src/components/GenerateImageForm.jsx
@@ -1,69 +1,137 @@
-
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from 'styled-components'
 import Button from './buttons/Button';
 import TextInput from './input/TextInput';
-import { AutoAwesome, CreateRounded } from '@mui/icons-material';
+import { AutoAwesome, CreateRounded } from "@mui/icons-material";
+
+
 
 const Form = styled.div`
-   flex: 1;
-   padding: 16px 20px;
-   display: flex;
-   flex-direction: column;
-   gap: 9%;
-   justify-content: center;
+  flex: 1;
+  padding: 16px 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 9%;
+  justify-content: center;
 `;
 const Top = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 6px;
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
 `;
 const Title = styled.div`
-    font-size: 28px;
-    font-weight: 500;
-    color: ${({theme}) => theme.text_primary};
+  font-size: 28px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.text_primary};
 `;
 const Desc = styled.div`
-    font-size: 17px;
-    font-weight: 400;
-    color: ${({theme}) => theme.text_secondary};
+  font-size: 17px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.text_secondary};
 `;
 const Body = styled.div`
-     display: flex;
-     flex-direction: column;
-     gap: 18px;
-     font-size: 12px;
-     font-weight: 400;
-     color: ${({theme}) => theme.text_secondary};
+  display: flex;
+  flex-direction: column;
+  gap: 18px;
+  font-size: 12px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.text_secondary};
 `;
 const Actions = styled.div`
-      flex: 1;
-      display: flex;
-      gap: 8px;
+  flex: 1;
+  display: flex;
+  gap: 8px;
 `;
 
-const GenerateImageForm = () => {
-  return <Form>
-    <Top>
+const GenerateImageForm = ({
+  post,
+  setPost,
+  createPostLoading,
+  setGenerateImageLoading,
+  generateImageLoading,
+  setCreatePostLoading,
+}) => {
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+  const generateImageFun = async () => {
+    setGenerateImageLoading(true);
+    await GenerateAIImage({ prompt: post.prompt })
+      .then((res) => {
+        setPost({
+          ...post,
+          photo: `data:image/jpge;base64,${res?.data?.photo}`,
+        });
+        setGenerateImageLoading(false);
+      })
+      .catch((error) => {
+        setError(error?.response?.data?.message);
+        setGenerateImageLoading(false);
+      });
+  };
+  const createPostFun = async () => {
+    setCreatePostLoading(true);
+    await CreatePost(post)
+      .then((res) => {
+        setCreatePostLoading(false);
+        navigate("/");
+      })
+      .catch((error) => {
+        setError(error?.response?.data?.message);
+        setCreatePostLoading(false);
+      });
+  };
+  return (
+    <Form>
+      <Top>
         <Title>Generate Image with prompt</Title>
         <Desc>
-            Write your prompt according to the image you want
+          Write your prompt according to the image you want to generate!
         </Desc>
-    </Top>
-    <Body>
-        <TextInput label="Author" placeholder="Enter your name.." name="name"/>
-        <TextInput label="Image Prompt" placeholder="Write detailed prompt about the image " 
-        name="name"
-        rows="8"
-        textArea
+      </Top>
+      <Body>
+        <TextInput
+          label="Author"
+          placeholder="Enter your name.."
+          name="name"
+          value={post.name}
+          handelChange={(e) => setPost({ ...post, name: e.target.value })}
         />
+        <TextInput
+          label="Image Prompt"
+          placeholder="Write a detailed prompt about the image . . . "
+          name="name"
+          rows="8"
+          textArea
+          value={post.prompt}
+          handelChange={(e) => setPost({ ...post, prompt: e.target.value })}
+        />
+        {error && <div style={{ color: "red" }}>{error}</div>}
         ** You can post the AI Generated Image to the Community **
-    </Body>
-   <Actions>
-    <Button text="Generate Image" flex leftIcon={<AutoAwesome/>}/>
-    <Button text="Post Image" flex type="secondary" leftIcon={<CreateRounded/>}/>
-    </Actions> 
-  </Form>
-}
+      </Body>
+      <Actions>
+        <Button
+          text="Generate Image"
+          flex
+          leftIcon={<AutoAwesome />}
+          isLoading={generateImageLoading}
+          isDisabled={post.prompt === ""}
+          onClick={() => generateImageFun()}
+        />
+        <Button
+          text="Post Image"
+          flex
+          type="secondary"
+          leftIcon={<CreateRounded />}
+          isLoading={createPostLoading}
+          isDisabled={
+            post.name === "" || post.prompt === "" || post.photo === ""
+          }
+          onClick={() => createPostFun()}
+        />
+      </Actions>
+    </Form>
+  );
+};
 
-export default GenerateImageForm
+export default GenerateImageForm;
--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,39 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import GenerateImageForm from '../components/GenerateImageForm';
 import GeneratedImageCard from '../components/GeneratedImageCard';
 
 const Container = styled.div`
-  height : 100%;
+  height: 100%;
   overflow-y: scroll;
+  background: ${({ theme }) => theme.bg};
   padding: 30px 30px;
   padding-bottom: 50px;
+  display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
   gap: 20px;
-  @media (max-width: 768){
-     padding: 6px 10px;
-     }
+  @media (max-width: 768px) {
+    padding: 6px 10px;
+  }
 `;
 
 const Wrapper = styled.div`
-  flex: 1;
-  height: fit-content;
   width: 100%;
+  height: fit-content;
   max-width: 1200px;
   gap: 8%;
   display: flex;
-  justify-content:center;
-  @media (max-width : 768px){
-  flex-direction: column;
+  justify-content: center;
+  @media (max-width: 768px) {
+    flex-direction: column;
   }
-`
+`;
 
 const CreatePost = () => {
-  return <Container><Wrapper>
-    <GenerateImageForm/>
-    <GeneratedImageCard/>
-    </Wrapper></Container>
+  const [generateImageLoading, setGenerateImageLoading] = useState(false);
+  const [createPostLoading, setCreatePostLoading] = useState(false);
+  const [post, setPost] = useState({
+    name: "",
+    prompt: "",
+    photo: "",
+  });
+  return (
+    <Container>
+      <Wrapper>
+        <GenerateImageForm
+          post={post}
+          setPost={setPost}
+          createPostLoading={createPostLoading}
+          setGenerateImageLoading={setGenerateImageLoading}
+          generateImageLoading={generateImageLoading}
+          setCreatePostLoading={setCreatePostLoading}
+        />
+        <GeneratedImageCard src={post?.photo} loading={generateImageLoading} />
+      </Wrapper>
+    </Container>
+  );
 };
 
-export default CreatePost
+export default CreatePost;
